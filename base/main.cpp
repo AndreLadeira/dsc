f2d9f7cced58149bef4857cb7dd79822815320ed
ladeira@MacAir <andreladeira.dev@gmail.com>
2018-12-11 15:37:12 -0200

    Removed old style casts

--- base/main.cpp
+++ base/main.cpp
@@ -1,24 +1,9 @@
 #include <iostream>
-#include <ctime>
-#include <iomanip>
-#include "teste.h"
 
 using namespace std;
 
 int main()
 {
-    cout<<"Starting...\n";
-
-    A a(17);
-    B b(17);
-    clock_t begin = clock();
-
-    randomize(a);
-    cout<< "Elapsed time: " << setprecision(3) <<
-           (clock() - begin) / (double) CLOCKS_PER_SEC << endl;
-
-    begin = clock();
-    randomize(b);
-    cout<< "Elapsed time: " << setprecision(3) <<
-           (clock() - begin) / (double) CLOCKS_PER_SEC << endl;
+    cout << "Hello World!" << endl;
+    return 0;
 }
