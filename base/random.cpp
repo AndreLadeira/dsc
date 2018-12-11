25d2b8396e65e1f825cea72a8249637aca1972c5
ladeira@MacAir <andreladeira.dev@gmail.com>
2018-12-11 16:13:31 -0200

    General improvements in the random facilities

--- base/random.cpp
+++ base/random.cpp
@@ -3,4 +3,5 @@
 namespace base
 {
     unsigned int g_seed;
+    const unsigned int rand_max = 0x7FFF;
 }
--- base/random.h
+++ base/random.h
@@ -5,20 +5,25 @@
 namespace base
 {
     extern unsigned int g_seed;
+    extern const unsigned int rand_max;
+
+    typedef int(*rand_fcn_t)(void);
+    typedef void(*srand_fcn_t)(unsigned int);
+
     inline void fast_srand(unsigned int seed)
     {
         g_seed = seed;
     }
+
     inline void fast_srand(void)
     {
         g_seed = static_cast<unsigned int>(time(nullptr));
     }
-    typedef int(*rand_fcn_t)(void);
 
     inline int fast_rand(void)
     {
         g_seed = (214013*g_seed+2531011);
-            return (g_seed>>16)&0x7FFF;
+        return (g_seed>>16)&rand_max;
     }
 }
 
