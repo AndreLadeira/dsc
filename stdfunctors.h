4b2d70bf96fbb9c5448b65672d11dc8974404c43
ladeira@Imac <andreladeira.dev@gmail.com>
2020-01-10 16:30:55 -0200

    Added default Update functors (default, ifGreater, ifSmaller

--- stdfunctors.h
+++ stdfunctors.h
@@ -5,8 +5,8 @@
 
 namespace core{
 
-template< typename delta_t = int,
-          typename Compare<delta_t>::compare_fcn_t compare = Compare<delta_t>::less >
+template< typename delta_t,
+          typename Compare<delta_t>::compare_fcn_t compare >
 class DeltaAcceptBestImprove : public core::DeltaAccept<delta_t, compare >
 {
 public:
@@ -14,9 +14,7 @@ public:
     DeltaAcceptBestImprove() = default;
     virtual ~DeltaAcceptBestImprove() = default;
 
-    using Base = core::DeltaAccept<delta_t, compare>;
-
-    virtual int operator()(const typename Base::delta_vector_t& delta_vec) const
+    virtual int operator()(const std::vector<delta_t>& delta_vec) const
     {
             int index = -1, i = 0;
             delta_t min_max = compare(0,1) ?
@@ -31,8 +29,14 @@ public:
     }
 };
 
-template< typename delta_t = int,
-          typename Compare<delta_t>::compare_fcn_t compare = Compare<delta_t>::less >
+template< typename delta_t = int>
+using DeltaAcceptBestIncrease = DeltaAcceptBestImprove<delta_t,Compare<delta_t>::greater>;
+
+template< typename delta_t = int>
+using DeltaAcceptBestDecrease = DeltaAcceptBestImprove<delta_t,Compare<delta_t>::less>;
+
+template< typename delta_t,
+          typename Compare<delta_t>::compare_fcn_t compare >
 class DeltaAccept1stImprove : public core::DeltaAccept<delta_t, compare >
 {
 public:
@@ -40,9 +44,7 @@ public:
     DeltaAccept1stImprove() = default;
     virtual ~DeltaAccept1stImprove() = default;
 
-    using Base = core::DeltaAccept<delta_t, compare>;
-
-    virtual int operator()(const typename Base::delta_vector_t& delta_vec) const
+    virtual int operator()(const std::vector<delta_t>& delta_vec) const
     {
             int i = 0;
             delta_t min_max = compare(0,1) ?
@@ -56,6 +58,40 @@ public:
             return -1;
     }
 };
+
+template< typename delta_t = int>
+using DeltaAccept1stIncrease = DeltaAcceptBestImprove<delta_t,Compare<delta_t>::greater>;
+
+template< typename delta_t = int>
+using DeltaAccept1stDecrease = DeltaAcceptBestImprove<delta_t,Compare<delta_t>::less>;
+
+template< typename solution_t,
+          typename objective_t,
+          typename Compare<objective_t>::compare_fcn_t compare>
+class DefaultUpdate : public core::Update<solution_t,objective_t,core::Compare<objective_t>::less>
+{
+public:
+
+    DefaultUpdate() = default;
+    virtual ~DefaultUpdate() = default;
+    virtual bool operator()(solution_t& bestSoFar, objective_t& bsfCost, const solution_t& candidate, const objective_t candidateCost ){
+        if ( compare(candidateCost, bsfCost) ){
+            bsfCost = candidateCost;
+            bestSoFar = candidate;
+            return true;
+        }
+        return false;
+    }
+
+};
+
+template< typename solution_t,
+          typename objective_t>
+using UpdateIfGreater = DefaultUpdate<solution_t,objective_t,Compare<objective_t>::greater >;
+
+template< typename solution_t,
+          typename objective_t>
+using UpdateIfsmaller = DefaultUpdate<solution_t,objective_t,Compare<objective_t>::less >;
 
 }
 
