6e5ffd1c05a1e37a7725dcd25c2f8d8a8ab1e2fd
ladeira@Imac <andreladeira.dev@gmail.com>
2020-01-10 16:34:45 -0200

    added stagnation trigger, added __RESULTGEN__ conditional compile directive

--- main.cpp
+++ main.cpp
@@ -54,11 +54,6 @@ int main(int, char * argv[])
     auto neighbors_calls_resettable = make_shared< core::NeighborCallsResettable<solution_t,transformation_t > >(neighbor);
     neighbor = neighbors_calls_resettable;
 
-    // Record intensification history between each create call
-    auto intsfRecorder = make_shared< core::IntesificationRecorder< solution_t > > (create_solution,
-                                                                                    *neighbors_calls_resettable);
-    create_solution = intsfRecorder;
-
     // OBJECTIVE FUNCTION AND ACCESSORIES
 
     shared_ptr< core::Objective<solution_t, problem_data_t> > cost =
@@ -66,9 +61,6 @@ int main(int, char * argv[])
 
     auto cost_call_counter = make_shared< core::ObjectiveCallsCounter<solution_t, problem_data_t> >(cost);
     cost = cost_call_counter;
-
-    auto progress_monitor = make_shared< core::ObjectiveProgress<solution_t, problem_data_t> >(cost);
-    cost = progress_monitor;
 
     // DELTA OBJECTIVE FUNCTION AND ACCESSORIES
 
@@ -85,9 +77,9 @@ int main(int, char * argv[])
     shared_ptr< core::DeltaAccept<> > accept;
 
     if ( argv[3] == nullptr )
-        accept = make_shared< core::DeltaAcceptBestImprove<> >();
+        accept = make_shared< core::DeltaAcceptBestDecrease<> >();
     else
-        accept = make_shared< core::DeltaAccept1stImprove<> >();
+        accept = make_shared< core::DeltaAccept1stDecrease<> >();
 
     // TRANSFORM FUNCTION AND ACCESSORIES
 
@@ -96,55 +88,80 @@ int main(int, char * argv[])
     core::ExecutionController exec;
     auto restarts = strtoul(argv[2],nullptr,0);
 
+    // UPDATE FUNCTION AND DECORATORS
+
+    shared_ptr< core::Update<solution_t,int,core::Compare<int>::less> > update;
+    update = make_shared< core::UpdateIfsmaller<solution_t,int> >();
+
+    auto progress_monitor = make_shared< core::ObjectiveProgress<solution_t,int,core::Compare<int>::less> >(update);
+    update = progress_monitor;
+
+    // Record intensification history between each create call
+    auto neighCallsRecorder = make_shared<
+        core::NeighborsCallsRecorder<solution_t,int,core::Compare<int>::less > > (update,*neighbors_calls_resettable);
+
+    update = neighCallsRecorder;
+
+    // add a stagnation counter. One stagnation count means intensifying to the end and not improving.
+    auto stagnation_counter = make_shared<
+        core::StagnationCounter<solution_t,int,core::Compare<int>::less > > (update);
+
+    update = stagnation_counter;
+
+
+
     // EXECUTION TIMER
     auto timer = make_shared<Timer>();
 
-    exec.addStopTrigger( make_shared< core::Trigger<> >(create_counter,restarts) );
+    //exec.addStopTrigger( make_shared< core::Trigger<> >(create_counter,restarts) );
     //exec.addStopTrigger( make_shared< core::Trigger<>>(neighbors_counter,800e03) );
     //exec.addStopTrigger( make_shared< core::Trigger<double>>(progress_monitor,0.65) );
 //    exec.addStopTrigger( make_shared< core::Trigger<>>(cost_call_counter,30) );
-    exec.addStopTrigger( make_shared< core::Trigger<double> >(timer,1.0) );
+    //exec.addStopTrigger( make_shared< core::Trigger<double> >(timer,1.0) );
+    exec.addStopTrigger( make_shared< core::Trigger<> >(stagnation_counter,300) );
 
     timer->start();
 
-    auto solution = (*create_solution)();
-    auto best = solution;
+    auto current = (*create_solution)();
+    auto best = current;
     auto best_cost = (*cost)(best);
-
-#ifdef __DEBUG__
-    const auto start_cost = best_cost;
+    auto current_cost = best_cost;
+#ifndef __RESULTGEN__
+    auto start_cost = current_cost;
 #endif
-
-
 
     while(!exec.stop())
     {
-        auto transitions = (*neighbor)(solution);
-        auto deltas = (*deltacost)(solution,transitions);
+        auto transitions = (*neighbor)(current);
+        auto deltas = (*deltacost)(current,transitions);
         auto index = (*accept)(deltas);
 
         if ( index > -1 )
         {
-            (*transform)(solution,transitions.at( static_cast<size_t>(index)) );
-
-            auto newcost = (*cost)(solution);
-            if ( newcost < best_cost )
-            {
-                best = solution;
-                best_cost = newcost;
-            }
+            (*transform)(current,transitions.at( static_cast<size_t>(index)) );
+            current_cost = current_cost + deltas.at( static_cast<size_t>(index) );
         }
         else
         {
-           solution = (*create_solution)();
+           (*update)(best,best_cost,current,current_cost);
+           current = (*create_solution)();
+           current_cost = (*cost)(current);
         }
     }
 
 #ifdef __DEBUG__
     assert( best_cost == (*cost)(best));
+#endif
 
+#ifdef __RESULTGEN__
+
+    std::cout<< std::setprecision(3) << best_cost << "\t" << progress_monitor->getValue() << "\t"
+             << cost_call_counter->getValue() << "\t" << neighbors_counter->getValue() << "\t"
+             << timer->getValue() << endl;
+    //std::cout<< neighCallsRecorder->getRecord() << endl;
+#else
     cout<< "Elapsed time: " << fixed << std::setprecision(2) << timer->getValue() << endl;
-    //std::cout<< "Initial result: " << start_cost << endl;
+    std::cout<< "Initial result: " << start_cost << endl;
     std::cout<< "Final result: " << best_cost << endl;
     std::cout<< "Improvement: " << progress_monitor->getValue() * 100.0 << "%\n";
     std::cout<< "Times create solution called: " << create_counter->getValue() << endl;
@@ -154,13 +171,11 @@ int main(int, char * argv[])
     cout.imbue( std::locale(""));
     std::cout<< "Times objective function was called: " << cost_call_counter->getValue() << endl;
     std::cout<< "Times delta function was called: " << delta_call_counter->getValue() << endl;
-    std::cout<< intsfRecorder->getRecord();
-#else
-
-    std::cout<< std::setprecision(3) << best_cost << "\t" << progress_monitor->getValue() << "\t"
-             << cost_call_counter->getValue() << "\t" << neighbors_counter->getValue() << "\t"
-             << timer->getValue() << endl;
-    //std::cout<< intsfRecorder->getRecord() << endl;
+    core::Statistics<> stats( neighCallsRecorder->getRecord() );
+    std::cout<< "Intensification until local maxima (min/max/avg/stddev):" << stats.getMin()
+             << "/" << stats.getMax() <<"/" << stats.getAverage() <<"/" << stats.getStdDev() << endl;
+    std::cout<< "Final stagnation count:" << stagnation_counter->getValue() << endl;
+    //std::cout<< neighCallsRecorder->getRecord() << endl;
 #endif
     return 0;
 }
