baa845a55cb3bc82b837963fa8e7b2a3e20d64f3
ladeira@Imac <andreladeira.dev@gmail.com>
2020-01-14 21:30:55 -0200

    Added Id facilities to Triggers.

--- execution_controller.cpp
+++ execution_controller.cpp
@@ -2,14 +2,25 @@
 
 using namespace core;
 
+__Trigger::__Trigger(std::string id):Identified<std::string>(id){}
+
+
+ExecutionController::ExecutionController():_stoppingTrigger("Error: no stop trigger set.")
+{
+
+}
+
 bool ExecutionController::restart(){
     for( auto trigger : restartTriggers )
         if (*trigger) return true;
     return false;
 }
 bool ExecutionController::stop(){
+
+    if ( stopTriggers.empty() ) return true;
+
     for( auto trigger : stopTriggers )
-        if (*trigger) return true;
+        if (*trigger) { _stoppingTrigger = trigger->getId(); return true; }
     return false;
 }
 
@@ -19,3 +30,9 @@ void ExecutionController::addRestartTrigger( trigger_ptr_t t){
 void ExecutionController::addStopTrigger( trigger_ptr_t t){
     stopTriggers.push_back(t);
 }
+
+std::string ExecutionController::getStoppingTrigger()
+{
+    return this->_stoppingTrigger;
+}
+
--- execution_controller.h
+++ execution_controller.h
@@ -6,8 +6,10 @@
 
 namespace core{
 
-struct __Trigger
+
+struct __Trigger : Identified<std::string>
 {
+    explicit __Trigger(std::string id);
     virtual ~__Trigger() = default;
     virtual operator bool() const = 0;
 };
@@ -15,8 +17,8 @@ struct __Trigger
 template <typename T = size_t>
 struct Trigger : public __Trigger
 {
-    Trigger(const std::shared_ptr<const Value<T>> valueptr, T limit , Compare<T> c = Compare<T>::greater ):
-        _valueptr(valueptr),_limit(limit), _compare(c){}
+    Trigger(std::string id, const std::shared_ptr<const Value<T>> valueptr, T limit, Compare<T> c = Compare<T>::greater ):
+        __Trigger(id),_valueptr(valueptr),_limit(limit),_compare(c){}
 
     virtual operator bool() const
     {
@@ -36,15 +38,19 @@ public:
 
     using trigger_ptr_t = std::shared_ptr<__Trigger>;
 
+    ExecutionController();
     bool restart();
     bool stop();
     void addRestartTrigger( trigger_ptr_t );
     void addStopTrigger( trigger_ptr_t );
+    std::string getStoppingTrigger();
 
-    private:
+private:
 
     std::vector<trigger_ptr_t> stopTriggers;
     std::vector<trigger_ptr_t> restartTriggers;
+
+    std::string _stoppingTrigger;
 };
 
 }
